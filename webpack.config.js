071bde67e12118fe934106ec983c30efec7ac3ee
Steven Rogers <steven.rogers1990@gmail.com>
2019-01-06 18:11:48 +0000

    Added dotenv-webpack for securing API keys

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: ["babel-polyfill", "./src/js/index.js"],
@@ -14,6 +15,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new Dotenv({
+            path: path.resolve(__dirname, './.env')
         })
     ],
     module: {
