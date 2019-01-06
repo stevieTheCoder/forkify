2519af1d83f2a7d126d89ca3f9d1bdaf5715eb5c
Steven Rogers <steven.rogers1990@gmail.com>
2019-01-06 17:10:26 +0000

    Further changes

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { key } from '../config';
 
 export default class Recipe {
     constructor(id) {
@@ -8,7 +7,7 @@ export default class Recipe {
 
     async getRecipe() {
         try {
-            const res = await axios(`https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`);
+            const res = await axios(`https://www.food2fork.com/api/get?key=${process.env.API_KEY}&rId=${this.id}`);
             this.title = res.data.recipe.title;
             this.author = res.data.recipe.publisher;
             this.img = res.data.recipe.image_url;
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { key } from '../config';
 
 export default class Search {
     constructor(query) {
@@ -8,7 +7,7 @@ export default class Search {
 
     async getResults() {
         try {
-            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            const res = await axios(`https://www.food2fork.com/api/search?key=${process.env.API_KEY}&q=${this.query}`);
         this.result = res.data.recipes;
         } catch (error) {
             console.log(error);
